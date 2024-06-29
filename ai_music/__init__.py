46e0a850a48a4b87b5c91acd3bab450fb7ad6b8a
Mayukh Maity <maitymayukh2003@gmail.com>
2024-06-29 17:58:57 +0530

    delete

--- ai_music/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'ai_music.apps.YourAppConfig'
--- ai_music/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name=''),  # Redirects to the index view function
-    path('convert/', views.image_to_music, name='convert'),
-    path('recommend/', views.recommend_music, name='recommend'),
-]
